b1c5053a78bf43ea78c317348d04b33668b300f7
MiguelJimenez <migl.jimnz55@gmail.com>
2025-02-19 11:42:23 -0500

    Más Salidas Por Consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,15 @@ public class Main {
         System.out.println("Hola :)");
         System.out.println("Estamos aprendiendo comandos Git");
         System.out.println("Repasando comandos");
+        System.out.println("Hola :)");
+        System.out.println("Estamos aprendiendo comandos Git");
+        System.out.println("Repasando comandos");
+        System.out.println("Hola :)");
+        System.out.println("Estamos aprendiendo comandos Git");
+        System.out.println("Repasando comandos");
+        System.out.println("Hola :)");
+        System.out.println("Estamos aprendiendo comandos Git");
+        System.out.println("Repasando comandos");
 
 
 
